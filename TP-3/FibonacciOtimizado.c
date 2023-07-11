1db08bb69708fdcdca0ba5fc187ecaa4b46b40ac
Álvaro Gomes <alvarogsilva.neto@gmail.com>
2023-07-10 21:27:07 -0300

    Update FibonacciOtimizado.c

--- TP-3/FibonacciOtimizado.c
+++ TP-3/FibonacciOtimizado.c
@@ -2,19 +2,15 @@
 
 #define MAX_SIZE 100
 
-long long int fibonacci[MAX_SIZE]; // Array para armazenar os valores calculados
+long long int fibonacci[MAX_SIZE]; 
 
 long long int FibonacciRecursivo(int n) {
-    if (n <= 1) {
+    if (n <= 1) 
         return n;
-    }
 
-    // Verificar se o valor já foi calculado
-    if (fibonacci[n] != -1) {
+    if (fibonacci[n] != -1) 
         return fibonacci[n];
-    }
 
-    // Calcular e armazenar o valor
     fibonacci[n] = FibonacciRecursivo(n - 1) + FibonacciRecursivo(n - 2);
     return fibonacci[n];
 }
@@ -22,15 +18,13 @@ long long int FibonacciRecursivo(int n) {
 int main() {
     int i, n = 20;
 
-    // Inicializar o array com valores -1 (indicando que ainda não foram calculados)
-    for (i = 0; i < MAX_SIZE; i++) {
+    for (i = 0; i < MAX_SIZE; i++) 
         fibonacci[i] = -1;
-    }
 
     printf("Série de Fibonacci:\n");
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) 
         printf("%lld ", FibonacciRecursivo(i));
-    }
+        
     printf("\n");
 
     return 0;
